8a9f60a30a7f637d9245c773b5f7235c2b0f9557
uvsq22011253 <71713869+uvsq22011253@users.noreply.github.com>
2021-02-19 15:26:51 +0100

    position feu

--- projet_incendie.py
+++ projet_incendie.py
@@ -9,18 +9,19 @@ WIDTH = 600
 HEIGHT = 600
 largeur_case = WIDTH // 20
 hauteur_case = HEIGHT // 20
+cpt = 0
 
 c = Canvas(window, width=WIDTH, height=HEIGHT, bg= "blue")
 
 def can (l) :
-    global color
+    global color, p
     for loop in range(200) :
         i = randint(1,18)
         j = randint(1, 18)
         liste[i][j] = 1
         if liste[i][j] == 1 :
             color = "#07680A"
-        c.create_rectangle((i * largeur_case, j * hauteur_case),
+        p = c.create_rectangle((i * largeur_case, j * hauteur_case),
                                 ((i + 1) * largeur_case, (j + 1) * hauteur_case), fill=color)
 
     return l
@@ -28,41 +29,59 @@ def can (l) :
 
 
 def can2 (l) :
-    global color
+    global color,p
     for loop in range(200) :
         i = randint(1,18)
         j = randint(1, 18)
-        if liste [i][j] == 0 :
+        if liste[i][j] == 0 :
             liste[i][j] = 2
         if liste[i][j] == 2 :
             color = "#E0EC02"
 
-        c.create_rectangle((i * largeur_case, j * hauteur_case),
+        p = c.create_rectangle((i * largeur_case, j * hauteur_case),
                                 ((i + 1) * largeur_case, (j + 1) * hauteur_case), fill=color)
 
     return l
 def can3 (l) :
-    global color
+    global color,p, hor, ver
     for loop in range(1) :
         i = randint(1, 18)
         j = randint(1, 18)
         liste[i][j] = 3
-        if liste[i][j] == 3 :
+
+        if liste[i][j] == 3:
             color = "orange"
-        c.create_rectangle((i * largeur_case, j * hauteur_case),
+        p = c.create_rectangle((i * largeur_case, j* hauteur_case),
                                 ((i + 1) * largeur_case, (j + 1) * hauteur_case), fill=color)
+        hor = i
+        ver = j
+        print (ver)
 
 
     return l
+
+
 liste = [20*[0] for i in range(20)]
 
 can(liste)
 can2(liste)
 can3(liste)
 print(liste)
+def ss () :
+    inn = 0
+    for i in liste:
+        for j in i :
+            if j == 3 :
+                print(i, j)
+
+    print(inn)
+
+
+
+
+
+ss()
 
 c.grid()
 
 window.mainloop()
-
-
